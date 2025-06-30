9bed06f897567536828986eba625797b01b3e3bd
Dhener Augusto <dhener.guss@gmail.com>
2025-06-30 14:05:14 -0300

    adicionando templates e pages

---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,32 @@
+import { PlayCircleIcon } from "lucide-react";
+import { DefaultInput } from "../DefaultInput";
+import { DefaultButton } from "../DefaultButton";
+import { Cycles } from "../Cycles";
+import styles from "./styles.module.css";
+
+export function MainForm() {
+  return (
+    <form className={styles.form} action="">
+      <div className={styles.formRow}>
+        <DefaultInput
+          labelText="task"
+          id="meuInput"
+          type="text"
+          placeholder="Digite Algo"
+        />
+      </div>
+
+      <div className={styles.formRow}>
+        <p>Nesse ciclo descanse por 5 min</p>
+      </div>
+
+      <div className={styles.formRow}>
+        <Cycles />
+      </div>
+
+      <div className={styles.formRow}>
+        <DefaultButton icon={<PlayCircleIcon />} color="green" />
+      </div>
+    </form>
+  );
+}
