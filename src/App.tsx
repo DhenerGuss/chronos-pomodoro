85e75aad2c7988af6ab928ed3a2ffe15539c1f0b
DhenerGuss <dhener.guss@gmail.com>
2025-07-01 21:14:55 -0300

    organizando os contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Home } from "./pages/Home";
-import { TaskContextProvider } from "./contexts/TaskContext";
+import { TaskContextProvider } from "./contexts/TaskContext/taskContextProvider";
 
 import "./styles/themes.css";
 import "./styles/global.css";
