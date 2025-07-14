6e49c104364da40a50e194f5b921f3872fb18aa0
Dhener Augusto <dhener.guss@gmail.com>
2025-07-14 16:22:34 -0300

    atualizando o contador a cada segundo no estado e na página

--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -1,19 +1,33 @@
 import type { TaskModel } from "../../models/TaskModel";
+import type { TaskStateModel } from "../../models/TaskStateModel";
 
 export enum TaskActionTypes {
   START_TASK = "START_TASK",
   INTERRUPT_TASK = "INTERRUPT_TASK",
   RESET_STATE = "RESET_STATE",
+  COUNT_DOWN = "COUNT-DOWN",
+  COMPLETE_TASK = "COMPLETE_TASK",
 }
 
-export type TaskActionModel =
+export type TaskActionWithPayload =
   | {
       type: TaskActionTypes.START_TASK;
-      playload: TaskModel;
+      payload: TaskModel;
+    }
+  | {
+      type: TaskActionTypes.COUNT_DOWN;
+      payload: { secondsRemaining: number };
+    };
+
+export type TaskActionWhitoutPayload =
+  | {
+      type: TaskActionTypes.RESET_STATE;
     }
   | {
       type: TaskActionTypes.INTERRUPT_TASK;
     }
   | {
-      type: TaskActionTypes.RESET_STATE;
+      type: TaskActionTypes.COMPLETE_TASK;
     };
+
+export type TaskActionModel = TaskActionWithPayload | TaskActionWhitoutPayload;
--- src/contexts/TaskContext/taskReducer.ts
+++ src/contexts/TaskContext/taskReducer.ts
@@ -36,9 +36,32 @@ export function taskReducer(
         }),
       };
     }
+    case TaskActionTypes.COMPLETE_TASK: {
+      return {
+        ...state,
+        activeTask: null,
+        secondsRemaining: 0,
+        formatedSecondsRemaining: "00:00",
+        tasks: state.tasks.map((task) => {
+          if (state.activeTask && state.activeTask.id === task.id) {
+            return { ...task, completeDate: Date.now() };
+          }
+          return task;
+        }),
+      };
+    }
 
     case TaskActionTypes.RESET_STATE: {
       return state;
+    }
+    case TaskActionTypes.COUNT_DOWN: {
+      return {
+        ...state,
+        secondsRemaining: action.payload.secondsRemaining,
+        formatedSecondsRemaining: formatSecondsToMinutes(
+          action.payload.secondsRemaining
+        ),
+      };
     }
   }
 
